3531c834d9d3ac1a4a0c3c8fd56f2e6d804000f3
Joel Aro <joelaro@outlook.com>
2015-04-11 18:50:56 -0400

    Downloading site data works, but need to download the weather data...

--- app/src/main/java/com/example/joel/mycaster/LocationXMLData.java
+++ app/src/main/java/com/example/joel/mycaster/LocationXMLData.java
@@ -7,10 +7,30 @@ import java.util.List;
  * Created by Joel on 4/11/2015.
  */
 public class LocationXMLData {
-    protected String code = null;
-    protected String nameEN = null;
-    protected String nameFR =  null;
-    protected String provinceCode = null;
+    protected int ID;
+    protected String code;
+    protected String nameEN;
+    protected String nameFR;
+    protected String provinceCode;
+
+    public LocationXMLData() {
+        ID = 0;
+        code = null;
+        nameEN = null;
+        nameFR =  null;
+        provinceCode = null;
+    }
+
+    public LocationXMLData(int i, String s1, String s2, String s3, String s4) {
+        ID = i;
+        code = s1;
+        nameEN = s2;
+        nameFR = s3;
+        provinceCode = s4;
+    }
+
+    public int getID() { return ID; }
+    public void setID(int i) { this.ID = i; }
 
     public String getCode() { return code; }
     public void setCode(String s) { this.code = s; }
--- app/src/main/java/com/example/joel/mycaster/MainActivity.java
+++ app/src/main/java/com/example/joel/mycaster/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.location.Location;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
@@ -11,7 +12,10 @@ import android.util.Log;
 import android.view.ContextMenu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.ListView;
 
 import org.xml.sax.InputSource;
@@ -27,6 +31,7 @@ import java.io.OutputStream;
 import java.io.Reader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.nio.channels.FileChannel;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -35,32 +40,44 @@ import javax.xml.parsers.SAXParserFactory;
 
 public class MainActivity extends Activity {
 
+    private static String UserLocation = null;
+    private static String UserLocationCode = null;
+    private static String UserLocationProvince = null;
+
     private static String baseurl   = "http://dd.weather.gc.ca/citypage_weather/xml";
     private static File xmldir      = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/dataset");
-    private static File xmlfile     = new File(xmldir + "/data.xml");
+    private static File xmlfile     = new File(xmldir + "/" + UserLocationCode + ".xml");
     private static File siteList    = new File(xmldir + "/siteList.xml");
+    private static File locationsPath   = new File(xmldir + "/locations.sqlite");
+
     public static List<LocationXMLData> locData;
     public static XMLData data;
     private static final int DIALOG_DOWNLOAD_PROGRESS = 1;
     private ProgressDialog pd;
     private enum ParseType { LOCATION_DATA, WEATHER_DATA }
+    private DBHandler db = new DBHandler(this);
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if(!siteList.exists()) {
+        //if(!siteList.exists()) {
             new DownloadData().execute("/siteList.xml");
-        } else {
-            parseData(ParseType.LOCATION_DATA);
-        }
+        //} //else {
+            //parseData(ParseType.LOCATION_DATA);
+        //}
 
+        /*ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, countries);
+        AutoCompleteTextView enterRegion = (AutoCompleteTextView) findViewById(R.id.regionfinder);
+        enterRegion.setAdapter(adapter);*/
+
+/*
         ListView lv = (ListView) findViewById(R.id.selectregion);
 
         ArrayAdapter<LocationXMLData> adapter = new ArrayAdapter<LocationXMLData>(this, android.R.layout.simple_list_item_1, locData);
         lv.setAdapter(adapter);
-        registerForContextMenu(lv);
+        registerForContextMenu(lv);*/
 
         //new DownloadData().execute("/data.xml");
     }
@@ -119,14 +136,61 @@ public class MainActivity extends Activity {
 
             if(s.equals("/siteList.xml"))
                 parseData(ParseType.LOCATION_DATA);
-            else
+            else {
                 parseData(ParseType.WEATHER_DATA);
+                Intent i = new Intent(getBaseContext(), DayView.class);
+                startActivity(i);
+                finish();
+            }
 
+            if(!locationsPath.exists()) {
+                for (LocationXMLData x : locData) {
+                    db.addLocation(x);
+                }
+            }
 
+            final List<LocationXMLData> dbLocationData = db.getAllLocations();
+            List<String> locNames = new ArrayList<String>();
 
-            Intent i = new Intent(getBaseContext(), DayView.class);
-            //startActivity(i);
-            //finish();
+            for(LocationXMLData d : dbLocationData) {
+                /*String log = "ID: " + d.getID()
+                        + " , CODE: " + d.getCode()
+                        + " , NAMEEN: " + d.getNameEN()
+                        + " , NAMEFR: " + d.getNameFR()
+                        + " , PROVINCE: " + d.getProvinceCode();*/
+                String log = "Name: " + d.getNameEN();
+                Log.d("STUFF", log);
+
+                //locNames[j++] = d.getNameEN();
+                locNames.add(d.getNameEN());
+            }
+
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(getBaseContext(), android.R.layout.simple_dropdown_item_1line, locNames);
+            AutoCompleteTextView enterRegion = (AutoCompleteTextView) findViewById(R.id.regionfinder);
+            enterRegion.setAdapter(adapter);
+            final Button regionBtn = (Button) findViewById(R.id.regionbutton);
+            enterRegion.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    regionBtn.setVisibility(View.VISIBLE);
+                    UserLocation = parent.getItemAtPosition(position).toString();
+                    for (LocationXMLData d : dbLocationData) {
+                        if(d.getNameEN().equals(UserLocation)) {
+                            UserLocationCode = d.getCode();
+                            UserLocationProvince = d.getProvinceCode();
+                            break;
+                        }
+                    }
+                }
+            });
+            regionBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.v("TAG: ", UserLocation + " " + UserLocationCode + " " + UserLocationProvince);
+                    new DownloadData().execute("/" + UserLocationCode + ".xml");
+                    parseData(ParseType.LOCATION_DATA);
+                }
+            });
         }
 
         @Override
@@ -180,6 +244,8 @@ public class MainActivity extends Activity {
         }
     }
 
+
+/*
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
         super.onCreateContextMenu(menu, v, menuInfo);
@@ -192,5 +258,5 @@ public class MainActivity extends Activity {
     @Override
     public boolean onContextItemSelected(MenuItem item) {
         return super.onContextItemSelected(item);
-    }
+    }*/
 }
